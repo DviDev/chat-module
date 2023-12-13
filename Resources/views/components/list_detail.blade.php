807ab475fb08abcbc9e3e1d5704c5108544a06f7
Davi Menezes <davimenezes.dev@gmail.com>
2023-12-13 02:43:55 -0300

    improvements

--- Resources/views/components/list_detail.blade.php
+++ Resources/views/components/list_detail.blade.php
@@ -1,6 +1,6 @@
 <div class="dark:text-gray-200 text-gray-800 px-4 space-y-1">
     <x-dvui::button.group class="text-sm border dark:border-gray-500 dark:text-gray-400 divide-x divide-gray-500">
-        <x-dvui::link text="categories" url="{{route('admin.chat.categories', $row->id)}}" teal class="rounded-l-md px-2 py-1"/>
+        <x-dvui::link text="categories" url="{{route('chat.categories', $row->id)}}" teal class="rounded-l-md px-2 py-1"/>
         <x-dvui::link text="configs" url="{{route('admin.chat.configs', $row->id)}}" teal class="px-2 py-1"/>
         <x-dvui::link text="group.permissions" url="{{route('admin.chat.group.permissions', $row->id)}}" teal class="px-2 py-1"/>
         <x-dvui::link text="participants" url="{{route('admin.chat.participants', $row->id)}}" teal class="px-2 py-1"/>
