03b1e9c35792eaf9949f3a00766fd135b143ff96
Davi Menezes <davimenezes.dev@gmail.com>
2024-10-21 15:32:50 -0300

    refactorings

--- Resources/views/components/page/chat_category_channel_topic_messages_page.blade.php
+++ Resources/views/components/page/chat_category_channel_topic_messages_page.blade.php
@@ -1,3 +1,10 @@
+@pushonce('livewire_styles')
+    @livewireStyles
+@endpushonce
+@pushonce('livewire_scripts')
+    @livewireScripts
+    @livewireScriptConfig
+@endpushonce
 <x-lte::layout.v1.page>
     <livewire:chat::channel.topic.messages-chat-page :topic="$topic"/>
 </x-lte::layout.v1.page>
