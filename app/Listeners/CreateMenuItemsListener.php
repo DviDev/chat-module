a7b1ead2f3e39c71fa0b040fcd137a3f1a741ead
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-07 01:26:08 -0300

    Remove unused `createMenuItem` method from `CreateMenuItemsListener`.

--- app/Listeners/CreateMenuItemsListener.php
+++ app/Listeners/CreateMenuItemsListener.php
@@ -3,18 +3,11 @@
 namespace Modules\Chat\Listeners;
 
 use Modules\Project\Contracts\CreateMenuItemsListenerContract;
-use Modules\Project\Models\ProjectModuleEntityDBModel;
-use Modules\Project\Models\ProjectModuleMenuModel;
 
 class CreateMenuItemsListener extends CreateMenuItemsListenerContract
 {
     public function moduleName(): string
     {
         return config('chat.name');
-    }
-
-    protected function createMenuItem(ProjectModuleMenuModel $menuModel, ?ProjectModuleEntityDBModel $entity = null, $active = null): void
-    {
-        parent::createMenuItem($menuModel, $entity, $active);
     }
 }
